89d216f000b965b24be0e36e36f946b1e8bc530b
josepo <jlpolo@gmail.com>
2022-08-27 18:58:36 +0200

    initial api test

---
+++ City.Tests/CityApiTest.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace City.Tests;
+
+public class CityApiTest : IClassFixture<WebApplicationFactory<Program>>
+{
+   private readonly HttpClient _client;
+
+   public CityApiTest(WebApplicationFactory<Program> factory)
+   {
+      _client = factory.CreateClient();
+   }
+
+   [Fact]
+   public async Task GetByName()
+   {
+      var response = await _client.GetAsync("city/granada");
+      response.EnsureSuccessStatusCode();
+   }
+}
