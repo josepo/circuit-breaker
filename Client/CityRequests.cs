cbdfed34ecbc9e8becd82f99c455d136353120c9
josepo <jlpolo@gmail.com>
2022-09-09 10:43:57 +0200

    improve console output

--- Client/CityRequests.cs
+++ Client/CityRequests.cs
@@ -32,7 +32,17 @@ public class CityRequests
 
    public override string ToString()
    {
-      return string.Join('\n',
-         _requests.Select(req => req.ToString()).ToArray());
+      var successes = _requests.Count(req => req.Status == Status.Success);
+      var fails = _requests.Count(req => req.Status == Status.Fail);
+
+      return
+         $"{_requests.Count()} requests ({successes} successes, {fails} fails)" +
+         $"\nwith an average running time of {AverageRunningTime()}ms \n\n" +
+         string.Join('\n', _requests.Select(req => req.ToString()).ToArray());
+   }
+
+   private double AverageRunningTime()
+   {
+      return Math.Round(_requests.Sum(req => req.RunningTime) / 10);
    }
 }
